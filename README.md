6d581799194c06715d10cc758241d66d6c431a85
ericady <ericadybull@gmail.com>
2023-11-30 11:45:47 +1000

    feat: add initial hardhat config

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # tsunami-test
+
+Write a “Vault” solidity contract
+
+Requirements:
+
+1. There should be a ‘deposit’, and ‘withdraw’ function that any user can use to deposit and withdraw any whitelisted ERC-20 token on the contract
+
+2. There should also be three additional functions that only admins can call.
+   i) ‘pause’ and ii) ‘unpause’ that prevent/enable new deposits or withdrawals from occurring.
+   iii) whitelistToken that admins call to whitelist tokens
+
+3. The code repository should contain testing for the contract as well.
+   i) The repository should also contain instructions in the readme for running tests
+
+4. The vault should be usable by any number of users
+
+The test will be evaluated on
+
+a) code cleanliness and clarity
+b) its readme
+c) if it runs as expected with tests
+d) The ease of a potential user interacting with the contract
+e) if it is tested properly
+f) if every requirement is met
---
+++ hardhat.config.ts
@@ -0,0 +1,63 @@
+import "@nomicfoundation/hardhat-ethers";
+import "@nomicfoundation/hardhat-verify";
+import "@nomicfoundation/hardhat-toolbox";
+import "@nomicfoundation/hardhat-network-helpers";
+import "hardhat-deploy";
+import "hardhat-abi-exporter";
+import "hardhat-contract-sizer";
+import { HardhatUserConfig } from "hardhat/config";
+
+import dotenv from "dotenv";
+dotenv.config();
+
+const config: HardhatUserConfig = {
+  solidity: {
+    version: "0.8.20",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+      outputSelection: {
+        "*": {
+          "*": ["storageLayout"],
+        },
+      },
+    },
+  },
+  networks: {},
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts",
+    deploy: "./scripts/deployment/deploy",
+    deployments: "./scripts/deployment/deployments",
+  },
+  contractSizer: {
+    alphaSort: false,
+    runOnCompile: false,
+    disambiguatePaths: false,
+  },
+  gasReporter: {
+    enabled: true,
+    currency: "USD",
+  },
+  abiExporter: {
+    path: "./abi",
+    clear: true,
+    runOnCompile: true,
+    flat: true,
+    spacing: 4,
+    pretty: false,
+  },
+  mocha: {
+    timeout: 120000,
+  },
+  typechain: {
+    outDir: "typechain",
+    target: "ethers-v6",
+  },
+};
+
+export default config;
